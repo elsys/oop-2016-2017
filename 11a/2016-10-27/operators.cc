2d62b179dc48a31951a7bd856b6b219b0afd4982
Emil Gotsev <egotsev@gmail.com>
2016-10-27 14:10:57 +0300

    Overloading operators * unary and binary operators * overloading inside and outside of the class body; the significance of the order of the arguments * overloading operator<< and why it's impossible to be predefined inside the class body

--- 11a/2016-10-27/operators.cc
+++ 11a/2016-10-27/operators.cc
@@ -3,81 +3,88 @@
 using namespace std;
 
 class Point {
-    double x_, y_;
-
+    int x_, y_;
+    
     public:
     
-    Point(double x, double y)
-            : x_(x), y_(y) {}
-
-    double get_x() const {
+    Point(int x, int y)
+        : x_(x), y_(y) {}
+    
+    int get_x() const {
         return x_;
     }
     
-    Point& set_x(double x) {
+    Point& set_x(int x) {
         x_ = x;
         return *this;
     }
     
-    double get_y() const {
+    int get_y() const {
         return y_;
     }
     
-    Point& set_y(double y) {
+    Point& set_y(int y) {
         y_ = y;
         return *this;
     }
-    
+
     void print() const {
         cout << "Point(" << x_ << ", "
-                         << y_ << ")" << endl;
+             << y_ << ")" << endl;
     }
-
+    
     Point plus(const Point& other) const {
         return Point(x_ + other.x_, y_ + other.y_);
     }
     
-    Point minus(const Point& other) const {
-        return Point(x_ - other.x_, y_ - other.y_);
-    }
-    
+    // binary +
     Point operator+(const Point& other) const {
         return plus(other);
     }
     
+    // unary -
     Point operator-() const {
         return Point(-x_, -y_);
     }
     
-    Point& operator +=(const Point& other) {
+    // +=
+    Point operator+=(const Point& other) {
         x_ += other.x_;
         y_ += other.y_;
         return *this;
     }
 };
 
+// binary -
 Point operator-(const Point& first, const Point& second) {
-    return first.minus(second);
+    return first + (-second);
+}
+
+// unary +
+Point operator+(const Point& point) {
+    return point;
 }
 
 ostream& operator<<(ostream& out, const Point& point) {
-    out << "Point(" << point.get_x() << ", "
-                    << point.get_y() << ") ";
+    out << "Print(" << point.get_x() << ", "
+                    << point.get_y() << ")";
     return out;
 }
 
-int main () {
+int main() {
     Point p1(10, 10);
-    Point p2(20, 20);
-    Point p3 = p1 + p2;
-    Point p4 = p1 - p2;
-    Point p5 = -p1 - p2;
-    p1 += p2;
-        
+    Point p2(12, 12);
+    
+    Point p3 = p2 + p1;
+    Point p4 = p2 - p1;
+    Point p5 = -p4;
+    Point p6 = +p5;
+    p6 += p2;
     cout << p1 << endl
          << p2 << endl
          << p3 << endl
          << p4 << endl
-         << p5 << endl;
+         << p5 << endl
+         << p6 << endl;
     return 0;
 }
