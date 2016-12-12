e93713e22c6ed925d594e5d26ab5936fbfca2a3f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-12-12 17:44:40 +0200

    full postfix example added...

---
+++ examples/postfix/plus.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "plus.hh"
+#include "context.hh"
+
+
+Plus::Plus()
+: BinaryOperation("+")
+{}
+
+
+double Plus::do_execute(double a1, double a2) const {
+	return a1+a2;
+}
+
+
+
+
+
+
+
